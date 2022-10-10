ee0de62f6bbc7bfe16d88dfc8ef4d9de5f79a0ef
PatchRequest <PatchRequest@protonmail.com>
2022-10-10 13:50:38 +0200

    maybe it can be down?

--- labwork/labwork02.py
+++ labwork/labwork02.py
@@ -1,22 +1,5 @@
 import itertools
 
-def test_restriction(rest, comb):
-    special_chars = "!@#$%^&*()_+-="
-    match rest:
-        case 'at_least_one_special_char':
-            return any(char in comb for char in special_chars)
-        case 'at_least_one_uppercase_char':
-            return any(char.isupper() for char in comb)
-        case 'at_least_one_lowercase_char':
-            return any(char.islower() for char in comb)
-        case 'at_least_one_digit':
-            return any(char.isdigit() for char in comb)
-        case 'no_consecutive_same_char':
-            return not any(char == comb[i+1] for i, char in enumerate(comb[:-1]))
-        case 'special_char_not_last_place':
-            return comb[-1] not in special_chars
-
-
 def handle_password_keyspace(assignment):
     alphabet = assignment["alphabet"]
     length = assignment["length"]
@@ -34,3 +17,19 @@ def handle_password_keyspace(assignment):
         if valid:
             count += 1
     return count
+
+def test_restriction(rest, comb):
+    special_chars = "!@#$%^&*()_+-="
+    match rest:
+        case 'at_least_one_special_char':
+            return any(char in comb for char in special_chars)
+        case 'at_least_one_uppercase_char':
+            return any(char.isupper() for char in comb)
+        case 'at_least_one_lowercase_char':
+            return any(char.islower() for char in comb)
+        case 'at_least_one_digit':
+            return any(char.isdigit() for char in comb)
+        case 'no_consecutive_same_char':
+            return not any(char == comb[i+1] for i, char in enumerate(comb[:-1]))
+        case 'special_char_not_last_place':
+            return comb[-1] not in special_chars
