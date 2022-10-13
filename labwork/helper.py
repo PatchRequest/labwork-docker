c0f93acf4a075ecaaad0cb726e6c975559bc6175
PatchRequest <PatchRequest@protonmail.com>
2022-10-13 13:17:26 +0200

    refactoring

--- labwork/helper.py
+++ labwork/helper.py
@@ -24,44 +24,19 @@ def multiply_with_alpha_in_gf2_128(x):
 def split_into_blocks(text, block_size):
     return [text[i:i+block_size] for i in range(0, len(text), block_size)]
 
-
-def encrypt_block(key, block,assignment_type):
-    # check if key is 16 bytes
-    if len(key) != 16:
-        print(key)
-        raise ValueError("Key must be 16 bytes long")
-
-    body = {
-        "operation": "encrypt",
-        "key": base64.b64encode(key).decode("utf-8"),
-        "plaintext": base64.b64encode(block).decode("utf-8")
-    }
-    
-    header = {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
-    result = session.post(oracle_url+assignment_type, data=json.dumps(body),headers=header)
-    if result.status_code != 200:
-        print(result.text)
-    result = result.json()
-
-    return base64.b64decode(result["ciphertext"])
-
-
-def decrypt_block(key, block,assignment_type):
-    body = {
-        "operation": "decrypt",
-        "key": base64.b64encode(key).decode("utf-8"),
-        "ciphertext": base64.b64encode(block).decode("utf-8")
-    }
-    header = {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
-    result = session.post(oracle_url+assignment_type, data=json.dumps(body),headers=header)
-    result = result.json()
-    return base64.b64decode(result["plaintext"])
-
 def byte_xor(ba1, ba2):
     return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)]) 
+
+def contact_oracle(key, block,assignment_type,mode):
+    body = {
+        "operation": mode,
+        "key": base64.b64encode(key).decode("utf-8"),
+        "plaintext" if mode == "encrypt" else "ciphertext": base64.b64encode(block).decode("utf-8")
+    }
+    header = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+    result = session.post(oracle_url+assignment_type, data=json.dumps(body),headers=header)
+    result = result.json()
+    return base64.b64decode(result["ciphertext" if mode == "encrypt" else "plaintext"])
--- labwork/labwork02.py
+++ labwork/labwork02.py
@@ -1,5 +1,5 @@
 import itertools
-from helper import multiply_with_alpha_in_gf2_128,split_into_blocks,encrypt_block,byte_xor,decrypt_block
+from helper import multiply_with_alpha_in_gf2_128,split_into_blocks,byte_xor,contact_oracle
 import base64
 
 
@@ -57,7 +57,6 @@ def handle_block_cipher(assignment):
         case "xex":
             return handle_xex(assignment)
   
-
 def handle_cbc(assignment):
     operation = assignment["operation"]
     iv = base64.b64decode(assignment["iv"])
@@ -65,12 +64,10 @@ def handle_cbc(assignment):
 
     if operation == "encrypt":
         plaintext = base64.b64decode(assignment["plaintext"])
-        return {"ciphertext": encrypt_cbc(iv, key, plaintext)}
+        return {"ciphertext": do_cbc(iv, key, plaintext,"encrypt")}
     else:
         ciphertext = base64.b64decode(assignment["ciphertext"])
-        return {"plaintext": decrypt_cbc(iv, key, ciphertext)}
-
-
+        return {"plaintext": do_cbc(iv, key, ciphertext,"decrypt")}
 
 def handle_ctr(assignment):
     key = base64.b64decode(assignment["key"])
@@ -90,74 +87,49 @@ def handle_xex(assignment):
     tweak = base64.b64decode(assignment["tweak"])
     if operation == "encrypt":
         plaintext = base64.b64decode(assignment["plaintext"])
-        return {"ciphertext": encrypt_xex(key, tweak, plaintext)}
+        return {"ciphertext": do_xex(key, tweak, plaintext,"encrypt")}
     else:
         ciphertext = base64.b64decode(assignment["ciphertext"])
-        return {"plaintext": decrypt_xex(key, tweak, ciphertext)}
+        return {"plaintext": do_xex(key, tweak, ciphertext,"decrypt")}
 
-def encrypt_cbc(iv, key,plaintext):
-    blocks = split_into_blocks(plaintext, 16)
+def do_cbc(iv,key,input,mode):
+    blocks = split_into_blocks(input, 16)
     currentIV = iv
-    ciphertext = b''
+    result = b''
     for block in blocks:
-        block = byte_xor(block, currentIV)
-        block = encrypt_block(key, block, "block_cipher")
-        currentIV = block
-        ciphertext += block
-    
-    return base64.b64encode(ciphertext).decode("utf-8")
+        if mode == "encrypt":
+            block = byte_xor(block, currentIV)
+            block = contact_oracle(key, block, "block_cipher","encrypt")
+            currentIV = block
+            result += block
+        else:
+            result += byte_xor(contact_oracle(key,block,"block_cipher","decrypt"), currentIV)
+            currentIV = block
+    return base64.b64encode(result).decode("utf-8")
 
-def decrypt_cbc(iv, key,ciphertext):
-    blocks = split_into_blocks(ciphertext, 16)
-    currentIV = iv
-    plaintext = b''
-    for block in blocks:
-        plaintext += byte_xor(decrypt_block(key, block, "block_cipher"), currentIV)
-        currentIV = block
-    return base64.b64encode(plaintext).decode("utf-8")
-
-def do_ctr(key, nonce, plaintext):
-    blocks = split_into_blocks(plaintext, 16)
+def do_ctr(key, nonce, input):
+    blocks = split_into_blocks(input, 16)
     ciphertext = b''
     for i, block in enumerate(blocks):
         counter = nonce + i.to_bytes(4, "big")
-        keystream = encrypt_block(key, counter, "block_cipher")
+        keystream = contact_oracle(key, counter, "block_cipher","encrypt")
         ciphertext += byte_xor(block, keystream)
     return base64.b64encode(ciphertext).decode("utf-8")
 
-def encrypt_xex(key, tweak, plaintext):
-    blocks = split_into_blocks(plaintext, 16)
+def do_xex(key,tweak, input, mode):    
+    blocks = split_into_blocks(input, 16)
     key1 = key[:16]
     key2 = key[16:]
-    ciphertext = b''
-    currentTweak = encrypt_block(key2, tweak, "block_cipher")
+    result = b''
+    currentTweak = contact_oracle(key2, tweak, "block_cipher","encrypt")
     for block in blocks:
         block = byte_xor(block, currentTweak)
-        block = encrypt_block(key1, block, "block_cipher")
+        block = contact_oracle(key1, block, "block_cipher",mode)
         block = byte_xor(block, currentTweak)
-        ciphertext += block
+        result += block
 
         currentTweak = int.from_bytes(currentTweak, "little")
         currentTweak = multiply_with_alpha_in_gf2_128(currentTweak)
         currentTweak = currentTweak.to_bytes(16, "little")
 
-    return base64.b64encode(ciphertext).decode("utf-8")
-
-
-def decrypt_xex(key, tweak, ciphertext):
-    blocks = split_into_blocks(ciphertext, 16)
-    key1 = key[:16]
-    key2 = key[16:]
-    plaintext = b''
-    currentTweak = encrypt_block(key2, tweak, "block_cipher")
-    for block in blocks:
-        block = byte_xor(block, currentTweak)
-        block = decrypt_block(key1, block, "block_cipher")
-        block = byte_xor(block, currentTweak)
-        plaintext += block
-
-        currentTweak = int.from_bytes(currentTweak, "little")
-        currentTweak = multiply_with_alpha_in_gf2_128(currentTweak)
-        currentTweak = currentTweak.to_bytes(16, "little")
-
-    return base64.b64encode(plaintext).decode("utf-8")
+    return base64.b64encode(result).decode("utf-8")
