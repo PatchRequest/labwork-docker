1a2bafe377d87f067564f49a58e8f316c45f8067
PatchRequest <PatchRequest@protonmail.com>
2022-10-20 19:32:33 +0200

    multi block fix

--- labwork/labwork03.py
+++ labwork/labwork03.py
@@ -4,25 +4,28 @@ from helper import split_into_blocks,check_padding_validity,byte_xor
 # attack pkcs7 with cbc
 def handle_pkcs7_padding(assignment):
     keyname = assignment["keyname"]
-    print(keyname)
     iv = base64.b64decode(assignment["iv"])
     ciphertext = base64.b64decode(assignment["ciphertext"])
-    # print ciphertext as hex
-    print("Ciphertext: " + ciphertext.hex())
+    print("\nCiphertext: " + keyname + "  " + ciphertext.hex()+"\n")
     return {"plaintext": attack_pkcs7_padding(keyname,iv, ciphertext)}
 
 
 def attack_pkcs7_padding(keyname,iv, ciphertext):
     blocks = split_into_blocks(ciphertext, 16)
-    
-
+    clearText = b''
+    current_iv = iv
     for block in blocks:
         kc = bytearray(b"\x00" * 16)
-        # for byte in block but reverse
         for current_byte in range(15, -1, -1):
             kc = bruteforce_new_kc_entry(kc,keyname,block,current_byte)
-            
-    return base64.b64encode(resolve_cbc(kc,iv,ciphertext)).decode("utf-8")
+        clearText += resolve_cbc(kc,current_iv)   
+        current_iv = block
+        
+    padding = clearText[-1]
+    clearText = clearText[:-padding]
+        
+    print("     " + clearText.hex())
+    return base64.b64encode(clearText).decode("utf-8")
 
 
 
@@ -30,8 +33,6 @@ def bruteforce_new_kc_entry(kc,keyname,ciphertext,current_byte):
     temp_iv = bytearray(b"\x00" * 16)
     for i in range(15, current_byte, -1):
         toGenerate = 16 - current_byte
-        #print("KC      : " + kc.hex())
-        #print("Generate: " + str(toGenerate))
         temp_iv[i] = kc[i] ^ toGenerate
     return brute_one_byte(kc,keyname,temp_iv,ciphertext,current_byte)
 
@@ -63,23 +64,16 @@ def brute_one_byte(kc,keyname,temp_iv,ciphertext,current_byte):
             print("Current kc: " + kc.hex() + "\n")
             return kc
 
-    #print("Failed to crack byte: " + str(current_byte))
-    #print("Used KC: " + kc.hex())
-    #print("Cipher: " + ciphertext.hex())
 
 
-
-def resolve_cbc(kc,iv,ciphertext):
-    cypher_blocks = split_into_blocks(ciphertext, 16)
-    kv_blocks = split_into_blocks(kc, 16)
-    clearText = b''
+def resolve_cbc(kc,iv):
+    kv = kc
     currentIV = iv
-    for i in range(0,len(cypher_blocks)):
-        print("XOR: " + currentIV.hex())
-        print("     " + kv_blocks[i].hex())
-        xored_thing = byte_xor(currentIV,kv_blocks[i])
-        padding = xored_thing[-1]
-        clearText += xored_thing[:-padding]
-        print("     " + clearText.hex())
-        currentIV = cypher_blocks[i]
+
+    print("XOR: " + currentIV.hex())
+    print("     " + kv.hex())
+    clearText = byte_xor(currentIV,kv)
+
+    
+       
     return clearText
